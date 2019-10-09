42ef329f4ff90bc935ae257ffa28f7f87a9a7d64
antoinehage <me@antoinehage.com>
2019-10-09 18:24:02 -0400

    first commit

---
+++ data/provision/tenants/info.js
@@ -0,0 +1,12 @@
+
+let guest = {
+	"_id": "5c0e74ba9acc3c5a84a51259",
+	"applications": {
+		"package": "DSBRD_GUEST",
+		"key": "a139786a6e6d18e48b4987e83789430b"
+	}
+};
+
+module.exports = {
+	guest
+};
